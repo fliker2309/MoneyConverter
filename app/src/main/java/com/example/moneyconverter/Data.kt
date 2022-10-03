a2467e20f727acc4665cd0bb3f7dd6d206e50d7c
Evgenii <evgeniikapchik@gmail.com>
2022-10-03 16:59:15 +0300

    convert png to webp add dataclass Currencies

---
+++ app/src/main/java/com/example/moneyconverter/Data.kt
@@ -0,0 +1,3 @@
+package com.example.moneyconverter
+
+class Data : ArrayList<DataItem>()
---
+++ app/src/main/java/com/example/moneyconverter/DataItem.kt
@@ -0,0 +1,31 @@
+package com.example.moneyconverter
+
+data class DataItem(
+
+    val CNY_in: String,
+    val CNY_out: String,
+
+    val EUR_in: String,
+    val EUR_out: String,
+
+    val PLN_in: String,
+    val PLN_out: String,
+
+    val RUB_EUR_in: String,
+    val RUB_EUR_out: String,
+
+    val RUB_in: String,
+    val RUB_out: String,
+
+    val UAH_in: String,
+    val UAH_out: String,
+
+    val USD_EUR_in: String,
+    val USD_EUR_out: String,
+
+    val USD_RUB_in: String,
+    val USD_RUB_out: String,
+
+    val USD_in: String,
+    val USD_out: String
+)
